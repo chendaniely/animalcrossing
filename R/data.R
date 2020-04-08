d15b482e56bcddd42100431fcc70d3dec5a793db
Daniel Chen <chendaniely@gmail.com>
2020-04-08 10:10:31 -0400

    animal crossing! :dog:

---
+++ R/data.R
@@ -0,0 +1,153 @@
+#' Animal Crossing: New Horizons bugs in the northern hemisphere
+#'
+#' A dataset containing
+#' the bug, image url, price, location, time, and month
+#'
+#' @format A data frame
+#' \describe{
+#' }
+#' @source \url{https://github.com/chendaniely/animal_crossing}
+"acnh_bugs_n"
+
+#' Animal Crossing: New Horizons bugs in the southern hemisphere
+#'
+#' A dataset containing
+#' the bug, image url, price, location, time, and month
+#'
+#' @format A data frame
+#' \describe{
+#' }
+#' @source \url{https://github.com/chendaniely/animal_crossing}
+"acnh_bugs_s"
+
+#' Animal Crossing: New Horizons crafting materials
+#'
+#' A dataset containing
+#' the crafting materials, image url, how to obtain, and price
+#'
+#' @format A data frame
+#' \describe{
+#' }
+#' @source \url{https://github.com/chendaniely/animal_crossing}
+"acnh_crafting"
+
+#' Animal Crossing: New Horizons equiptment diy recipes
+#'
+#' A dataset containing
+#' the equiptment, image url, materials needed, size, how it is obtained, and price
+#'
+#' @format A data frame
+#' \describe{
+#' }
+#' @source \url{https://github.com/chendaniely/animal_crossing}
+"acnh_diy_equipment"
+
+#' Animal Crossing: New Horizons housewares diy recipes
+#'
+#' A dataset containing
+#' the houseware, image url, materials needed, size, how it is obtained, and price
+#'
+#' @format A data frame
+#' \describe{
+#' }
+#' @source \url{https://github.com/chendaniely/animal_crossing}
+"acnh_diy_housewares"
+
+#' Animal Crossing: New Horizons housewares diy recipes
+#'
+#' A dataset containing
+#' the houseware, image url, materials needed, size, how it is obtained, and price
+#'
+#' @format A data frame
+#' \describe{
+#' }
+#' @source \url{https://github.com/chendaniely/animal_crossing}
+"acnh_diy_housewares"
+
+#' Animal Crossing: New Horizons miscellaneous diy recipes
+#'
+#' A dataset containing
+#' the item, image url, materials needed, size, how it is obtained, and price
+#'
+#' @format A data frame
+#' \describe{
+#' }
+#' @source \url{https://github.com/chendaniely/animal_crossing}
+"acnh_diy_misc"
+
+#' Animal Crossing: New Horizons other diy recipes
+#'
+#' A dataset containing
+#' the item, image url, materials needed, size, how it is obtained, and price
+#'
+#' @format A data frame
+#' \describe{
+#' }
+#' @source \url{https://github.com/chendaniely/animal_crossing}
+"acnh_diy_other"
+
+#' Animal Crossing: New Horizons tool diy recipes
+#'
+#' A dataset containing
+#' the tool, image url, materials needed, size, how it is obtained, and price
+#'
+#' @format A data frame
+#' \describe{
+#' }
+#' @source \url{https://github.com/chendaniely/animal_crossing}
+"acnh_diy_tools"
+
+#' Animal Crossing: New Horizons wall, floring, and rug diy recipes
+#'
+#' A dataset containing
+#' the item, image url, materials needed, size, how it is obtained, and price
+#'
+#' @format A data frame
+#' \describe{
+#' }
+#' @source \url{https://github.com/chendaniely/animal_crossing}
+"acnh_diy_wallfloorrug"
+
+#' Animal Crossing: New Horizons wallmount recipes
+#'
+#' A dataset containing
+#' the item, image url, materials needed, size, how it is obtained, and price
+#'
+#' @format A data frame
+#' \describe{
+#' }
+#' @source \url{https://github.com/chendaniely/animal_crossing}
+"acnh_diy_wallmount"
+
+#' Animal Crossing: New Horizons fish in the northern hemisphere
+#'
+#' A dataset containing
+#' the fish, image url, price, location, time, and month
+#'
+#' @format A data frame
+#' \describe{
+#' }
+#' @source \url{https://github.com/chendaniely/animal_crossing}
+"acnh_fish_n"
+
+#' Animal Crossing: New Horizons fish in the southern hemisphere
+#'
+#' A dataset containing
+#' the fish, image url, price, location, time, and month
+#'
+#' @format A data frame
+#' \describe{
+#' }
+#' @source \url{https://github.com/chendaniely/animal_crossing}
+"acnh_fish_s"
+
+#' Animal Crossing: New Horizons villagers
+#'
+#' A dataset containing
+#' the villager, image url, personality, species, birtday, and catchphrase
+#'
+#' @format A data frame
+#' \describe{
+#' }
+#' @source \url{https://github.com/chendaniely/animal_crossing}
+"acnh_villagers"
--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 R package for animal crossing data
 
 See here for the data collection scripts: https://github.com/chendaniely/animal_crossing
+
+## Installation
+
+```r
+if (!require("remotes")){install.packages("remotes")}
+remotes::install_github("chendaniely/animalcrossing")
+```
